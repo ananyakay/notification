00e3d6f75fe17ca5f9b46ad1e0a8e8404f69430a
ananya <ananya.prakashpatil05@gmail.com>
2023-07-13 18:37:27 +0530

    second-commit

--- txapp/settings.py
+++ txapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,10 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'txapp.urls'
 
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'txhelpdesk/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,6 +144,5 @@ REST_FRAMEWORK = {
     ]
 }
 
-LOGIN_URL = 'registration/login.html'
    
 LOGIN_REDIRECT_URL = 'txhelpdesk:index'
--- txhelpdesk/views.py
+++ txhelpdesk/views.py
@@ -1,36 +1,34 @@
-from rest_framework import generics, viewsets
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.decorators import action
-
-from txhelpdesk.models import Notification
-from txhelpdesk.serializers import NotificationSerializer
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics, viewsets
+from txhelpdesk.models import Notification
+from rest_framework.permissions import IsAuthenticated
+from txhelpdesk.serializers import NotificationSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
+
 
 @login_required()
 def index(request):
-    return render(request, 'txhelpdesk/login.html')
+    return render(request, 'registration/login.html')
 
-@login_required()
-def base(request):
+def index(request):
     return render(request, 'txhelpdesk/base.html')
 
 class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
-    permission_classes = (IsAuthenticated,)
-
+    
     def get_queryset(self):
         user = self.request.user
         return Notification.objects.filter(user=user)
     
-
-    # code i added
     @action(detail=True, methods=['put'])
     def mark_as_read(self, request, pk=None):
         notification = self.get_object()
         notification.read = True
         notification.save()
         return Response(status=200)
+    
+
+    
